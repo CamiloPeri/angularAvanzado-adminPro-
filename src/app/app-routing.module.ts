78d6ecfe8cb06dc7ea9470e18f1f7ed4632577bc
Camilo Soler <camilosoler@cbit-online.com>
2020-09-25 13:23:44 -0500

    [Cssc;#develop;feat]: cambio en rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,12 @@ import { AuthRoutingModule } from './auth/auth.routing';
 import { NopagesfoundComponent } from './nopagesfound/nopagesfound.component';
 import { PagesRoutingModule } from './pages/pages.routing';
 
-const routes: Routes = [{ path: '**', component: NopagesfoundComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NopagesfoundComponent },
+
+
+];
 
 @NgModule({
   declarations: [],
